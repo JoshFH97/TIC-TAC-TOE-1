63e7adc396ebe055c84a823454373f102f2a7716
JoshFH97 <joshelxd@gmail.com>
2024-05-21 15:53:12 -0600

    javascript

--- fx.js
+++ fx.js
@@ -1,6 +1,6 @@
 const cuadrados = document.querySelectorAll('.cuadro');
-const x = '👌'
-const o = '👍'
+const x = document.ge
+const o = 'O'
  let turn = 'player1'
 
 
@@ -8,7 +8,7 @@ const o = '👍'
 cuadrados.forEach((cuadrado,i)=> {
     cuadrado.addEventListener('click', () => {
     cuadrado.innerText = turn === 'player1' ? x : o;
-    turn =  turn === 'player1' ? 'player2' : 'player1';
+    turn =  turn === 'player1' ? 'computer' : 'player1';
     //console.log(cuadrado)
     revisarGanador()
 
@@ -23,10 +23,38 @@ function revisarGanador() {
     console.log(tablero)
     
     //verticales
-    
-
-    //horizontales
+        if (tablero[0]!=''&&tablero[0] == tablero[1] && tablero[0] == tablero[2]) {
+            alert('Winner vertical 1')
+        }
+        if (tablero[3] != '' && tablero[3] == tablero[4] && tablero[3] == tablero[5]) {
+            alert('Winner vertical 2')
+        }
+        if (tablero[6]!=''&&tablero[6] == tablero[7] && tablero[6] == tablero[8]) {
+            alert('Winner vertical 3')
+        }
+        
+   //horizontales
+        if (tablero[0]!=''&& tablero[0] == tablero[3] && tablero[0] == tablero[6]) {
+            alert('Winner horizontal 1')
+        }
+        if (tablero[1] != '' && tablero[1] == tablero[4] && tablero[1] == tablero[7]) {
+            alert('Winner horizontal 2')
+        }
+        if (tablero[2]!='' && tablero[2] == tablero[5] && tablero[2] == tablero[8]) {
+            alert('Winner horizontal 3')
+        }
+        
 
     //diagonales
+    if (tablero[0]!=''&&tablero[0] == tablero[4] && tablero[0] == tablero[8]) {
+        alert('Winner diagonales 1')
+    }
+    if (tablero[6] != '' && tablero[6] == tablero[4] && tablero[6] == tablero[2]) {
+        alert('Winner diagonales 2')
+    }
     
 }
+/*function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+  }*/
+  
